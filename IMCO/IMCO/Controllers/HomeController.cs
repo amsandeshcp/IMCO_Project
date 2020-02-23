b06a3e277dc6420af99a2158bffb687e9138d215
VisualDesigners <amsandeshcp@gmail.com>
2020-02-23 11:01:50 +0530

    Base class Attached on base folder

--- IMCO/IMCO/Controllers/HomeController.cs
+++ IMCO/IMCO/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IMCO.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,10 +7,11 @@ using System.Web.Mvc;
 
 namespace IMCO.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
+           DownloadFile_Isprint();
             return View();
         }
 
